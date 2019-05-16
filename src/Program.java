45db9ab440d9c96e51295821c7de77f6fb2edac3
Vladimir <larsoni4@rambler.ru>
2019-05-16 21:40:47 +0300

    Честно, так и не понял я как сделать циклическую форму именно быстрого возведения в степень.

--- src/Program.java
+++ src/Program.java
@@ -1,36 +1,54 @@
-import java.util.Iterator;
-import java.util.LinkedList;
-
 public class Program {
+
     public static void main(String[] args) {
-        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
-        linkedList.addFirst(10);
-        linkedList.addFirst(20);
-        linkedList.addFirst(30);
-        linkedList.addFirst(40);
-        linkedList.addFirst(50);
-        System.out.println(linkedList);
-        System.out.println(linkedList.size());
-        System.out.println(linkedList.get(3));
-        linkedList.set(1, 100);
-        System.out.println(linkedList.get(1));
-        System.out.println(linkedList);
-        linkedList.addFirst(60);
-        linkedList.addLast(70);
-        linkedList.add(2, 80);
-        System.out.println(linkedList);
-        linkedList.removeLast();
-        linkedList.removeFirst();
-        System.out.println(linkedList.size());
-        System.out.println(linkedList);
-        System.out.println(" ");
 
-        for (Integer item :
-                linkedList) {
-            System.out.print(item + ", ");
-        }
-
-        LinkedList<Integer> list = new LinkedList<>();
-        Iterator<Integer> iterator = list.iterator();
+        System.out.println(expo(5, 4));
+        System.out.println(expoRec(5, 4));
+        hanoiRec(4, 1, 2, 3);
     }
+
+    public static int expoRec(int n, int x) {
+        if (x == 0) return 1;
+        if (x % 2 == 1) return expoRec(n, x - 1) * n;
+        else {
+            int b = expoRec(n, x / 2);
+            return b * b;
+        }
+    }
+
+    public static int expo(int n, int x) {
+        int result = n;
+        if (x == 0) return 1;
+        if (x == 1) return n;
+        while (x > 1) {
+            result *= n;
+            --x;
+        }
+        return result;
+    }
+
+    public static void hanoiRec(int n, int from, int to, int additional) {
+        if (n == 0) return;
+        hanoiRec(n - 1, from, additional, to);
+        System.out.println(from + " " + to);
+        hanoiRec(n - 1, additional, to, from);
+    }
+
+    public static void hanoi() {
+        int n = 4;
+        int from = 0;
+        int to = 2;
+        int additional = 1;
+
+        /*
+        сначало облазил пол интернета в поиске помощи, потом почитал я логику нерекурсивного метода вот здесь:
+        http://algolist.manual.ru/maths/combinat/hanoi.php
+        и в итоге понял, что это тяжело для меня пока что. смысл я понимаю полностью, но как реализовать это
+        я просто без понятия..
+        я так понял, что нам нужно будет использоваить стэк, который будем обновлять
+        после каждой итерации(перекладывания кольца).
+         */
+
+    }
+
 }
