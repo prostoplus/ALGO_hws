c3a4b80e4d98ef53c27c2560986da02e1711c5fa
Vladimir <larsoni4@rambler.ru>
2019-05-23 23:07:15 +0300

    Доброго время дня! Я бы ещё хотел скинуть вам картинку своего графа, просто я строил рёбра отталкиваясь от того, что сам себе нарисовал. Все свои пометки оставил коментами в самой программе!

--- src/Program.java
+++ src/Program.java
@@ -1,26 +1,33 @@
-import java.security.SecureRandom;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Program {
 
     public static void main(String[] args) {
-        List<MyTreeMap> treeList = new LinkedList<>();
-        SecureRandom random = new SecureRandom();
-        for (int i = 0; i < 20; i++) {
-            MyTreeMap<Integer, Integer> myTreeMap = new MyTreeMap<>();
-            for (int j = 0; j < random.nextInt(12); j++) {
-                myTreeMap.put(j++, 100 - random.nextInt(201));
-            }
-            treeList.add(myTreeMap);
-        }
 
-        int count = 0;
-        for (int i = 0; i < treeList.size(); i++) {
-            count += treeList.get(i).isHeightBalanced() ? 1 : 0;
-        }
+        Graph graph = new Graph(10);
 
-        System.out.println(String.format("Total trees count: " + treeList.size()));
-        System.out.println(String.format("Balanced: " + (count * 100 / treeList.size()) + " percent's"));
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(3, 5);
+        graph.addEdge(2, 4);
+        graph.addEdge(4, 6);
+        graph.addEdge(6, 7);
+        graph.addEdge(5, 7);
+        graph.addEdge(7, 8);
+        graph.addEdge(7, 9);
+        graph.addEdge(8, 9);
+
+        BreadthFirstSearch bfs = new BreadthFirstSearch(graph, 7);
+
+        System.out.println(bfs.hasPathTo(3));
+        System.out.println(bfs.pathTo(3));
+        System.out.println(bfs.distTo(3));
+
+        /*
+        Вроде бы как всё понятно, вроде бы и не сильно сложно, но над каждым движением думаю по 30 минут)
+        ЕСЛИ ВОЗМОЖНО, очень большая просьба уделить время сложным местам в теме про Графы, т.к. заходит гораздо сложнее деревьев.
+        В проге всё работает, в дебаге проверял каждую итерацию, чтобы понять алгоритм работы, но всё равно мало въезжаю.
+        Так же хотелось бы уделить хотя бы минут 5 поиску кротчайшего пути, а то чисто на бумаге, когда себе рисую я понимаю, но
+        в проге я совсем туплю, не могу научиться мыслить как программист(..
+         */
     }
 }
